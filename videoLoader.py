928c7c915ae7a3b4c3cfa26eb761630a47aff912
LuisReyes <lfernandoacevedo57@gmail.com>
2023-03-21 08:26:44 -0600

    update project

--- videoLoader.py
+++ videoLoader.py
@@ -1,17 +1,20 @@
 import cv2
 from detector.detector_placas_v2 import detector_placas
+import numpy as np
+import imutils
 
 capture = cv2.VideoCapture('C:/Users/Gerardo/Downloads/Untitled.avi')
 aux_placa = ""
 while (capture.isOpened()):
     ret, frame = capture.read()
+    frame = imutils.resize(frame,width=1080)
     if (ret == True):
         cv2.imshow("gato0", frame)
         placa = detector_placas(frame)
         if(placa != None):
             aux_placa = placa
             print("placa= ",placa)
-        if (cv2.waitKey(30) == ord('s')):
+        if (cv2.waitKey(100) == ord('s')):
             break
     else:
         break
